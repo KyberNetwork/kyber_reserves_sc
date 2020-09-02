5f89146a6aa2fc767f8235f613ec2df9286fcb32
Desmond Ho <howeilun@hotmail.com>
2020-09-02 17:53:13 +0800

    Lint mock files, fix lints

--- contracts/sol6/mock/ReserveGasProfiler.sol
+++ contracts/sol6/mock/ReserveGasProfiler.sol
@@ -3,7 +3,8 @@ pragma solidity 0.6.6;
 import "../IKyberReserve.sol";
 
 interface IPricing {
-    function getRate(IERC20 token, uint currentBlockNumber, bool buy, uint qty) external view returns(uint);
+    function getRate(IERC20 token, uint currentBlockNumber, bool buy, uint qty)
+        external view returns(uint);
 }
 
 contract ReserveGasProfiler {
@@ -13,7 +14,9 @@ contract ReserveGasProfiler {
         bool buy,
         uint256 srcQty
     ) external view returns (uint256) {
-        return pricing.getRate(token, block.number, buy, srcQty);
+        uint256 gasAmt = gasleft();
+        pricing.getRate(token, block.number, buy, srcQty);
+        return gasAmt - gasleft();
     }
 
     function profileReserveRate(
@@ -22,6 +25,8 @@ contract ReserveGasProfiler {
         IERC20 dest,
         uint256 srcQty
     ) external view returns (uint256) {
-        return reserve.getConversionRate(src, dest, srcQty, block.number);
+        uint256 gasAmt = gasleft();
+        reserve.getConversionRate(src, dest, srcQty, block.number);
+        return gasAmt - gasleft();
     }
 }
