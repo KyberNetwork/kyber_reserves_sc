78afa145956c8f2d0dc9d8b7f512091a508f8f1a
Desmond Ho <howeilun@hotmail.com>
2020-09-11 15:42:28 +0800

    Switch coverage order because sol6 mock token got removed

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,7 @@
 yarn buidler clean
 rm -r ./.tempCoverageArtifacts
 
-# Coverage sequence is sol4 -> sol6 -> sol5
+# Coverage sequence is sol4 -> sol5 -> sol6
 yarn buidler coverage --config ./buidlerConfigSol4.js --testfiles "test/sol4/*.js" --solcoverjs ".solcover.js"
-yarn buidler coverage --config ./buidlerConfigSol6.js --testfiles "test/sol6/*.js" --solcoverjs ".solcover.js"
 yarn buidler coverage --config ./buidlerConfigSol5.js --testfiles "test/sol5/*.js" --solcoverjs ".solcover.js"
+yarn buidler coverage --config ./buidlerConfigSol6.js --testfiles "test/sol6/*.js" --solcoverjs ".solcover.js"
