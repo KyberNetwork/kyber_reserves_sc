06b7e4a91c00146b38ac3efe81c629cf32a069e9
ducquangkstn <ducquang.kstn@gmail.com>
2020-10-22 10:23:31 +0700

    update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -9,9 +9,14 @@ yarn hardhat clean
 
 if [ "$VERSION" == 'sol4' ]; then
   echo "Running sol4 coverage..."
+<<<<<<< HEAD
   yarn hardhat coverage --testfiles "test/sol4/*.js" --solcoverjs ".solcover.js"
+=======
+  yarn buidler coverage --config ./buidlerConfigSol4.js --testfiles "test/sol4/" --solcoverjs ".solcover.js"
+>>>>>>> update coverage.sh
 elif [ "$VERSION" == 'sol5' ]; then
   echo "Running sol5 coverage..."
+<<<<<<< HEAD
   yarn hardhat coverage --testfiles "test/sol5/*.js" --solcoverjs ".solcover.js"
 elif [ "$VERSION" == 'sol6' ]; then
   echo "Running sol6 coverage..."
@@ -19,4 +24,13 @@ elif [ "$VERSION" == 'sol6' ]; then
 else
   echo "Running full coverage..."
   yarn hardhat coverage --solcoverjs ".solcover.js"
+=======
+  yarn buidler coverage --config ./buidlerConfigSol5.js --testfiles "test/sol5/" --solcoverjs ".solcover.js"
+else
+  echo "Running sol6 coverage..."
+  yarn buidler compile --config buidlerConfigSol5.js &&
+  yarn buidler compile --config buidlerConfigSol4.js
+  cp -R ./artifacts/ ./.tempCoverageArtifacts
+  yarn buidler coverage --config ./buidlerConfigSol6.js --testfiles "test/sol6/" --solcoverjs ".solcover.js"
+>>>>>>> update coverage.sh
 fi
