77ef00c07746d064a32a6b4c5fc6dcf7032fbae2
Mike <manhlx.300695@gmail.com>
2020-12-31 00:23:45 +0700

    Fix some comments

--- contracts/sol6/wrappers/WrapReadTokenDataV2.sol
+++ contracts/sol6/wrappers/WrapReadTokenDataV2.sol
@@ -17,24 +17,16 @@ contract WrapReadTokenDataV2 is Withdrawable {
     )
         external view
         returns (
-            int numBuyRateQtyStepsX, 
             int[] memory buyRateQtyStepsX,
-            int numBuyRateQtyStepsY,
             int[] memory buyRateQtyStepsY,
-            int numSellRateQtyStepsX,
             int[] memory sellRateQtyStepsX,
-            int numSellRateQtyStepsY,
             int[] memory sellRateQtyStepsY
         )
     {
-        (numBuyRateQtyStepsX, buyRateQtyStepsX) =
-            _getStepFunctionData(rate, token, 0);
-        (numBuyRateQtyStepsY, buyRateQtyStepsY) =
-            _getStepFunctionData(rate, token, 2);
-        (numSellRateQtyStepsX, sellRateQtyStepsX) =
-            _getStepFunctionData(rate, token, 4);
-        (numSellRateQtyStepsY, sellRateQtyStepsY) =
-            _getStepFunctionData(rate, token, 6);
+        buyRateQtyStepsX = _getStepFunctionData(rate, token, 0);
+        buyRateQtyStepsY = _getStepFunctionData(rate, token, 2);
+        sellRateQtyStepsX = _getStepFunctionData(rate, token, 4);
+        sellRateQtyStepsY = _getStepFunctionData(rate, token, 6);
     }
 
     function readImbalanceStepFunctions(
@@ -43,39 +35,28 @@ contract WrapReadTokenDataV2 is Withdrawable {
     )
         external view
         returns (
-            int numBuyRateImbalanceStepsX,
             int[] memory buyRateImbalanceStepsX,
-            int numBuyRateImbalanceStepsY,
             int[] memory buyRateImbalanceStepsY,
-            int numSellRateImbalanceStepsX,
             int[] memory sellRateImbalanceStepsX,
-            int numSellRateImbalanceStepsY,
             int[] memory sellRateImbalanceStepsY
         )
     {
-        (numBuyRateImbalanceStepsX, buyRateImbalanceStepsX) =
-            _getStepFunctionData(rate, token, 8);
-        (numBuyRateImbalanceStepsY, buyRateImbalanceStepsY) =
-            _getStepFunctionData(rate, token, 10);
-        (numSellRateImbalanceStepsX, sellRateImbalanceStepsX) =
-            _getStepFunctionData(rate, token, 12);
-        (numSellRateImbalanceStepsY, sellRateImbalanceStepsY) =
-            _getStepFunctionData(rate, token, 14);
+        buyRateImbalanceStepsX = _getStepFunctionData(rate, token, 8);
+        buyRateImbalanceStepsY = _getStepFunctionData(rate, token, 10);
+        sellRateImbalanceStepsX = _getStepFunctionData(rate, token, 12);
+        sellRateImbalanceStepsY = _getStepFunctionData(rate, token, 14);
     }
 
     function _getStepFunctionData(
         IConversionRatesGetSteps rate,
         IERC20Ext token,
         uint command
-    ) internal view
-        returns(
-            int numSteps,
-            int[] memory stepValues
-        )
+    )
+        internal view returns(int[] memory stepValues)
     {
-        numSteps = rate.getStepFunctionData(token, command, 0);
-        stepValues = new int[](uint(numSteps));
-        for(uint i = 0; i < uint(numSteps); i++) {
+        uint numSteps = uint(rate.getStepFunctionData(token, command, 0));
+        stepValues = new int[](numSteps);
+        for(uint i = 0; i < numSteps; i++) {
             stepValues[i] = rate.getStepFunctionData(token, command + 1, i);
         }
     }
