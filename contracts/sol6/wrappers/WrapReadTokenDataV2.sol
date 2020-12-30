0d444502628d8955d6a2a12255065f690a6ad6d5
Mike <manhlx.300695@gmail.com>
2020-12-30 21:00:35 +0700

    Simplify the contract

--- contracts/sol6/wrappers/WrapReadTokenDataV2.sol
+++ contracts/sol6/wrappers/WrapReadTokenDataV2.sol
@@ -27,34 +27,14 @@ contract WrapReadTokenDataV2 is Withdrawable {
             int[] memory sellRateQtyStepsY
         )
     {
-        uint i;
-        numBuyRateQtyStepsX = rate.getStepFunctionData(token, 0, 0);
-        buyRateQtyStepsX = new int[](uint(numBuyRateQtyStepsX));
-
-        for (i = 0; i < uint(numBuyRateQtyStepsX); i++) {
-            buyRateQtyStepsX[i] = rate.getStepFunctionData(token, 1, i);
-        }
-
-        numBuyRateQtyStepsY = rate.getStepFunctionData(token, 2, 0);
-        buyRateQtyStepsY = new int[](uint(numBuyRateQtyStepsY));
-
-        for (i = 0; i < uint(numBuyRateQtyStepsY); i++) {
-            buyRateQtyStepsY[i] = rate.getStepFunctionData(token, 3, i);
-        }
-
-        numSellRateQtyStepsX = rate.getStepFunctionData(token, 4, 0);
-        sellRateQtyStepsX = new int[](uint(numSellRateQtyStepsX));
-
-        for (i = 0; i < uint(numSellRateQtyStepsX); i++) {
-            sellRateQtyStepsX[i] = rate.getStepFunctionData(token, 5, i);
-        }
-
-        numSellRateQtyStepsY = rate.getStepFunctionData(token, 6, 0);
-        sellRateQtyStepsY = new int[](uint(numSellRateQtyStepsY));
-
-        for (i = 0; i < uint(numSellRateQtyStepsY); i++) {
-            sellRateQtyStepsY[i] = rate.getStepFunctionData(token, 7, i);
-        }
+        (numBuyRateQtyStepsX, buyRateQtyStepsX) =
+            _getStepFunctionData(rate, token, 0);
+        (numBuyRateQtyStepsY, buyRateQtyStepsY) =
+            _getStepFunctionData(rate, token, 2);
+        (numSellRateQtyStepsX, sellRateQtyStepsX) =
+            _getStepFunctionData(rate, token, 4);
+        (numSellRateQtyStepsY, sellRateQtyStepsY) =
+            _getStepFunctionData(rate, token, 6);
     }
 
     function readImbalanceStepFunctions(
@@ -73,32 +53,30 @@ contract WrapReadTokenDataV2 is Withdrawable {
             int[] memory sellRateImbalanceStepsY
         )
     {
-        uint i;
-        numBuyRateImbalanceStepsX = rate.getStepFunctionData(token, 8, 0);
-        buyRateImbalanceStepsX = new int[](uint(numBuyRateImbalanceStepsX));
+        (numBuyRateImbalanceStepsX, buyRateImbalanceStepsX) =
+            _getStepFunctionData(rate, token, 8);
+        (numBuyRateImbalanceStepsY, buyRateImbalanceStepsY) =
+            _getStepFunctionData(rate, token, 10);
+        (numSellRateImbalanceStepsX, sellRateImbalanceStepsX) =
+            _getStepFunctionData(rate, token, 12);
+        (numSellRateImbalanceStepsY, sellRateImbalanceStepsY) =
+            _getStepFunctionData(rate, token, 14);
+    }
 
-        for (i = 0; i < uint(numSellRateImbalanceStepsX); i++) {
-            buyRateImbalanceStepsX[i] = rate.getStepFunctionData(token, 9, i);
-        }
-
-        numBuyRateImbalanceStepsY = rate.getStepFunctionData(token, 10, 0);
-        buyRateImbalanceStepsY = new int[](uint(numBuyRateImbalanceStepsY));
-        for (i = 0; i < uint(numBuyRateImbalanceStepsY); i++) {
-            buyRateImbalanceStepsY[i] = rate.getStepFunctionData(token, 11, i);
-        }
-
-        numSellRateImbalanceStepsX = rate.getStepFunctionData(token, 12, 0);
-        sellRateImbalanceStepsX = new int[](uint(numSellRateImbalanceStepsX));
-
-        for (i = 0; i < uint(numSellRateImbalanceStepsX); i++) {
-            sellRateImbalanceStepsX[i] = rate.getStepFunctionData(token, 13, i);
-        }
-
-        numSellRateImbalanceStepsY = rate.getStepFunctionData(token, 14, 0);
-        sellRateImbalanceStepsY = new int[](uint(numSellRateImbalanceStepsY));
-
-        for (i = 0; i < uint(numSellRateImbalanceStepsY); i++) {
-            sellRateImbalanceStepsY[i] = rate.getStepFunctionData(token, 15, i);
+    function _getStepFunctionData(
+        IConversionRatesGetSteps rate,
+        IERC20Ext token,
+        uint command
+    ) internal view
+        returns(
+            int numSteps,
+            int[] memory stepValues
+        )
+    {
+        numSteps = rate.getStepFunctionData(token, command, 0);
+        stepValues = new int[](uint(numSteps));
+        for(uint i = 0; i < uint(numSteps); i++) {
+            stepValues[i] = rate.getStepFunctionData(token, command + 1, i);
         }
     }
 }
